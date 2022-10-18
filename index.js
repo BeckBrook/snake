c69b299f0c2aa92cd64ee53466ffe65f3545970b
BeckBrook <beckbrookdrawing@gmail.com>
2022-10-18 16:28:37 +0200

    test

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello");
+console.log('fils de iench');
+
+
+/*
+const canvas = document.getElementById('game'); //the HTML element which id is game is defined as a constant
+const ctx = canvas.getContext('2d'); //we define the context in 2D (we could use 3D for example)
+
+let speed = 7; //snake's speed
+
+function drawGame(){//game loop to update the screen in real time
+  console.log('draw game');
+  setTimeout(drawGame, 1000/ speed); //1000ms makes 1s
+}
+
+drawGame();
+*/
